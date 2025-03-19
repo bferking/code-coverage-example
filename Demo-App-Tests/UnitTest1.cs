5a89ec2e77396dad67a790ea410d2caae66c2cf5
Blake Ferkingstad <108814@nelnet.net>
2025-03-19 16:26:11 -0500

    Add coverage

--- Demo-App-Tests/UnitTest1.cs
+++ Demo-App-Tests/UnitTest1.cs
@@ -33,4 +33,19 @@ public class UnitTest1
         var output = writer.GetStringBuilder().ToString().Trim();
         Assert.Equal($"Howdy, UNIT TEST!{Environment.NewLine}{Environment.NewLine}Complete", output);
     }
+
+    [Fact]
+    public void OneArguments_Test_Expect_additional()
+    {
+        // Arrange
+        var writer = new StringWriter();
+        Console.SetOut(writer);
+
+        // Act
+        Program.Main(["TEST"]);
+
+        // Assert
+        var output = writer.GetStringBuilder().ToString().Trim();
+        Assert.Equal($"Howdy, TEST!{Environment.NewLine}{Environment.NewLine}Calling from another method{Environment.NewLine}Complete", output);
+    }
 }
