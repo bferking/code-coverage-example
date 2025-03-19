6c923fa46ad3427338bb8521cbe959fa1feae94e
Blake Ferkingstad <108814@nelnet.net>
2025-03-19 10:11:15 -0500

    Adding coverage for one path

--- Demo-App-Tests/UnitTest1.cs
+++ Demo-App-Tests/UnitTest1.cs
@@ -4,25 +4,18 @@ namespace Demo_App_Tests;
 
 public class UnitTest1
 {
-    // Will add back later
-    //[Fact]
-    //public void NoArguments_Test()
-    //{
-    //    // Arrange
-    //    var writer = new StringWriter();
-    //    Console.SetOut(writer);
-
-    //    // Act
-    //    Program.Main(Array.Empty<string>());
-
-    //    // Assert
-    //    var output = writer.GetStringBuilder().ToString().Trim();
-    //    Assert.Equal("Hello!", output);
-    //}
-
-    [Fact]
-    public void Bad_Test()
+   [Fact]
+    public void NoArguments_Test()
     {
-        Assert.Equal(true, true);
+        // Arrange
+        var writer = new StringWriter();
+        Console.SetOut(writer);
+
+        // Act
+        Program.Main(Array.Empty<string>());
+
+        // Assert
+        var output = writer.GetStringBuilder().ToString().Trim();
+        Assert.Equal("Hello there!", output);
     }
 }
