19ce2365989fd8665e3d872c4ce0c6cb990e813d
Blake Ferkingstad <108814@nelnet.net>
2025-03-24 11:16:17 -0500

    Adding uncovered lines

--- Demo-App/Program.cs
+++ Demo-App/Program.cs
@@ -22,6 +22,9 @@ public class Program
         if (name.ToLower().Equals("test"))
         {
             Console.WriteLine("Calling from another method");
+        } else if (name.ToLower().Equals("New Branch"))
+        {
+            Console.WriteLine("This is a new uncovered area");
         }
     }
 }
