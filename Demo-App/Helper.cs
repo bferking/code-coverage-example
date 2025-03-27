c858a7ac92f52f3d5eeb34a41d5d825a2d16c378
Blake Ferkingstad <108814@nelnet.net>
2025-03-27 15:29:53 -0500

    Changing message

--- Demo-App/Helper.cs
+++ Demo-App/Helper.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Demo_App
+﻿namespace Demo_App
 {
     class Helper
     {
@@ -16,7 +10,7 @@ namespace Demo_App
             }
             else if (name.ToLower().Equals("New Branch"))
             {
-                Console.WriteLine("This is a new uncovered area");
+                Console.WriteLine("This is a new uncovered area.");
             }
         }
     }
