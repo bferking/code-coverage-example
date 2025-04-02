f6a3c3b2f21ecee8bbf73e9a40e813054f3d22cd
Blake Ferkingstad <108814@nelnet.net>
2025-04-02 08:42:32 -0500

    Adding test but adding more uncovered lines to see how report handles it

---
+++ Demo-App-Tests/HowdyServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using Demo_App;
+
+namespace Demo_App_Tests;
+
+public class HowdyServiceTests
+{
+    [Fact]
+    public void Howdy_Test()
+    {
+        // Arrange
+        var howdyService = new HowdyService();
+        // Act
+        var result = howdyService.Howdy("UNIT TEST");
+        // Assert
+        Assert.Equal("Howdy UNIT TEST", result);
+    }
+}
--- Demo-App/HowdyService.cs
+++ Demo-App/HowdyService.cs
@@ -1,17 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Demo_App
+﻿namespace Demo_App
 {
-    class HowdyService
+    public class HowdyService
     {
 
         public string Howdy(string name)
         {
             return "Howdy " + name;
+        }
+
+        public string Hello(string name)
+        {
+            return "Hello " + name;
         }
     }
 }
