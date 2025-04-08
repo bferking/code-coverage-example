a0b2d918c843753c05efe98c2e8a5a653a6eea5a
Blake Ferkingstad <108814@nelnet.net>
2025-04-08 09:05:10 -0500

    Add additional coverage for hola

--- Demo-App-Tests/HowdyServiceTests.cs
+++ Demo-App-Tests/HowdyServiceTests.cs
@@ -2,16 +2,33 @@
 
 namespace Demo_App_Tests;
 
-public class HowdyServiceTests
+public class HowdyServiceTests : IDisposable
 {
+    private readonly HowdyService _howdyService;
+
+    public HowdyServiceTests()
+    {
+        _howdyService = new HowdyService();
+    }
+
+    public void Dispose()
+    {
+        
+    }
+
     [Fact]
     public void Howdy_Test()
     {
-        // Arrange
-        var howdyService = new HowdyService();
         // Act
-        var result = howdyService.Howdy("UNIT TEST");
+        var result = _howdyService.Howdy("UNIT TEST");
         // Assert
         Assert.Equal("Howdy UNIT TEST", result);
+   }
+
+    [Fact]
+    public void Howdy_Hola_Test()
+    {
+        var result = _howdyService.Hola("UNIT TEST");
+        Assert.Equal("Hola UNIT TEST", result);
     }
 }
